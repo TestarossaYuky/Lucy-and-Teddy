7129f7f53020835a0377721243afec0897d0b795
Hugo <h.lagarde@student.isartdigital.com>
2019-02-06 10:42:59 +0100

    Add new ladder Ai system

---
+++ PRODUCTION/Assets/Scripts/Ladder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    [SerializeField]
+    private float aiChance = 0.7f;
+
+    [SerializeField]
+    private float secondChance = 0.2f;
+
+    private bool first = false;
+    private float baseChance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseChance = aiChance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float GetAiChance()
+    {
+        return this.aiChance;
+    }
+
+    public void SetAiChance(float chance)
+    {
+        aiChance = chance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ennemi")
+        {
+            if (!first)
+            {
+                SetAiChance(secondChance);
+                first = true;
+            }
+            else
+            {
+                SetAiChance(baseChance);
+                first = false;
+            }
+                
+        }
+    }
+}
